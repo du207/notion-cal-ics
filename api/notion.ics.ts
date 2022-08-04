6289bcf8f7416c06e2c62d27ea2d2a68b0b80ccf
dwsong07 <dwsong07@naver.com>
2022-08-04 16:49:53 +0900

    Made it to vercel function

---
+++ api/notion.ics.ts
@@ -0,0 +1,19 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import generateIcs from "../src/generateIcs";
+import rateLimiter from "lambda-rate-limiter";
+
+const limit = rateLimiter({
+    interval: 10000,
+}).check;
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+    try {
+        await limit(1, request.headers["x-real-ip"] as string);
+    } catch (err) {
+        return response.send(429);
+    }
+
+    const ics = await generateIcs();
+
+    response.setHeader("content-type", "text/plain").send(ics);
+};
--- src/generateIcs.ts
+++ src/generateIcs.ts
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import { createEvents } from "ics";
 import { readNotionCal } from "notion-cal-lib";
-import { writeFile } from "fs/promises";
-import { notify } from "node-notifier";
 import { dateArray, dateTimeArray, getNextDay } from "./utils";
 
-const syncToIcs = async () => {
+export default async function () {
     const token = process.env.NOTION_TOKEN;
     const database_id = process.env.DATABASE_ID;
 
@@ -14,7 +12,7 @@ const syncToIcs = async () => {
 
     const notionCal = await readNotionCal(token, database_id);
 
-    createEvents(
+    const { error, value } = createEvents(
         notionCal.map(({ title, date: { start, end } }) => {
             let startArray, endArray;
             if (start.includes("T")) {
@@ -35,28 +33,14 @@ const syncToIcs = async () => {
                 startOutputType: "local",
                 endOutputType: "local",
             };
-        }),
-        async (err, value) => {
-            if (err) throw err;
-
-            const withTZ = value
-                .replaceAll("DTSTART:", "DTSTART;TZID=Asia/Seoul:")
-                .replaceAll("DTEND:", "DTEND;TZID=Asia/Seoul:");
-
-            await writeFile("./notion.ics", withTZ);
-        }
+        })
     );
-};
 
-const main = async () => {
-    try {
-        await syncToIcs();
-    } catch (err) {
-        console.error(err);
-        notify({
-            title: "Notion ICS Syncing Error",
-            message: "Check the log",
-        });
-    }
-};
-main();
+    if (error || !value) throw error;
+
+    const icsWithTZ = value
+        .replaceAll("DTSTART:", "DTSTART;TZID=Asia/Seoul:")
+        .replaceAll("DTEND:", "DTEND;TZID=Asia/Seoul:");
+
+    return icsWithTZ;
+}
